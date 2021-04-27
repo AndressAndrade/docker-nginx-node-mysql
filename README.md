e9d7bd62a31a1cecb832883fd49d5de026848afa
andressandrade <andressa.brasil-tokyo@hotmail.com>
2021-04-27 19:01:18 -0300

    Subindo imagens

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # docker-nginx-node-mysql
-Desafio 1 - Estudo de docker
+
+Desafio - Configurar um container nginx para que ao ser acessado, mostre os dados de uma aplicação rodando em um container interno que por sua vez obtem dados de um container contendo um banco de dados mysql
+
+## Como rodar
+
+- Clone o repositório na sua máquina
+(A imagem nodeapi está no [Docker Hub](https://hub.docker.com/r/andressandrade/nodeapi) )
+
+- Crie uma rede bridge para os containers se comunicarem <br/> `docker network create --driver bridge nodeapi`
+
+- Acesse a pasta mysql e inicie o container do mysql
+<br/> `docker run -d --rm --name mysql -e MYSQL_ROOT_PASSWORD=root MYSQL_DATABASE=fullcycle -v $(pwd)/data:/var/lib/mysql  --network nodeapi andressandrade/mysql`
+
+- Acesse a pasta nodeapi, e inicie o container da nodeapi <br/> `docker run --name nodeapi -v "$(pwd):/usr/scr/app" -d --network nodeapi andressandrade/nodeapi`
+
+- Acesse a pasta nginx-revese, e inicie o container do nginx <br/> `docker run -p 8080:80 -d --network nodeapi  andressandrade/nginx `
+
+- Acesse http://localhost:8080
+
+## Resultado
+
---
+++ nodeapi/src/index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+
+const config = {
+    host: "mysql",
+    user: "root",
+    password: "root",
+    database: "fullcycle"
+};
+
+const connection = mysql.createConnection(config);
+// const insertSQL = `INSERT INTO modulos(name) values('Docker'), ('Comunicação'), ('RabbitMQ'), ('Apache Kafka')`;
+// connection.query(insertSQL);
+// connection.end();
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    const query = `SELECT * FROM modulos`;
+    connection.query(query, function (error, results) {
+        // When done with the connection, release it.
+        connection.end();
+     
+        // Handle error after the release.
+        if (error) {
+            return res.status(500).json({
+                message: 'Erro ao obter lista',
+                status: false,
+                erro: error.message
+            });
+        };
+     
+        let texto = '<h1>Full Cycle</h1><ul>';
+        results.forEach(element => {
+            texto += `<li>${element.name}</li>`;
+        });
+        texto += '</ul>';
+        return res.status(200).send(texto);
+        
+      });
+});
+
+app.listen(port, () => {
+    console.log("Rodando na porta: ", port);
+});
