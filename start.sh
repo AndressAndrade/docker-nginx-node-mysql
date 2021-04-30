f219434574afa140e3b73ff4ecd3d746a29f635a
andressandrade <andressa.brasil-tokyo@hotmail.com>
2021-04-29 22:40:40 -0300

    Otimizando codigo

--- start.sh
+++ start.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
+
 docker network create nodeapi
 
-docker run -d --rm --name mysql -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=fullcycle --network nodeapi andressandrade/mysql
-
-sleep 60
-
-docker exec -i mysql mysql -uroot -proot < ./mysql/insert.sql
+docker run -d --rm --name mysql --network nodeapi andressandrade/mysql
 
 docker run --rm --name nodeapi -d --network nodeapi andressandrade/nodeapi  
 
 docker run --rm -p 8080:80 -d --network nodeapi  andressandrade/nginx
+
+sleep 5
 
 echo "Acesse: http://localhost:8080"
